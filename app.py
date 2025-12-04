b56c64c41043d4fa840d3324c1a03ed8ae203513
rneto464 <steveray12e@gmail.com>
2025-12-04 00:58:58 -0300

    Remover duplicação de import os e BASE_DIR em app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import json
 
 # --- Configurações do Flask ---
 # Configurar caminhos absolutos para static e templates
-import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
